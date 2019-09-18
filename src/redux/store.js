2d255bbadf6cf22b8c0fbbac4cf81074f0430228
Johan IBTATEN <j.ibtaten@hotmail.fr>
2019-09-18 20:07:34 +0200

    updating code to leverage containers for collections-overview and collection-page components

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,28 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { persistStore } from 'redux-persist';
 //import logger from 'redux-logger';
-
+import thunk from 'redux-thunk';
 import rootReducer from './root-reducer';
 
-//const middlewares = [logger];
 
-//export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk),
+  // other store enhancers if any
+);
+//const store = createStore(reducer, enhancer);
+
+
 
 export const store = createStore(
-    rootReducer, 
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    rootReducer,
+    enhancer
 );
 
 export const persistor = persistStore(store);
