ce79c4ad4c947dc855a4db9818f52dde40c8da11
Johan IBTATEN <j.ibtaten@hotmail.fr>
2019-09-23 10:14:26 +0200

    creating a redux-saga for fetching our collections from the firestore database

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,30 +1,34 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import { persistStore } from 'redux-persist';
-//import logger from 'redux-logger';
-import thunk from 'redux-thunk';
-import rootReducer from './root-reducer';
+import logger from 'redux-logger';
+//import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
+import rootReducer from './root-reducer';
+import rootSaga from './root-saga';
+
+const sagaMiddleware = createSagaMiddleware();
 
 const composeEnhancers =
   typeof window === 'object' &&
   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
     }) : compose;
 
 const enhancer = composeEnhancers(
-  applyMiddleware(thunk),
-  // other store enhancers if any
+  applyMiddleware(sagaMiddleware, logger),
 );
-//const store = createStore(reducer, enhancer);
-
-
 
 export const store = createStore(
     rootReducer,
     enhancer
 );
 
+sagaMiddleware.run(rootSaga);
+
 export const persistor = persistStore(store);
 
 export default { store, persistStore };
+
+
+
