0199dd16caf85d3a5f37c2b2e3d265f6f4be1405
Johan IBTATEN <j.ibtaten@hotmail.fr>
2019-10-09 13:21:28 +0200

    Converting all our code over to be mobile friendly

--- client/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.styles.jsx
+++ client/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.styles.jsx
@@ -1,13 +1,18 @@
 import styled from 'styled-components';
 
 export const SignInAndSignUpContainer = styled.div`
-  /* width: 850px;
+  width: 850px;
   display: flex;
   justify-content: space-between;
-  margin: 30px auto; */
-  margin: 0 auto;
-  max-width: 850px;
-  width: 90vw;
-  display: flex;
-  justify-content: space-between;
+  margin: 30px auto;
+
+  @media screen and (max-width: 800px) {
+    flex-direction: column;
+    width: unset;
+    align-items: center;
+
+    > *:first-child {
+      margin-bottom: 50px;
+    }
+  }
 `;
